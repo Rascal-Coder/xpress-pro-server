e104e66cf30959d0fad536aec3e572beb68ad6f5
rascal <ras-cal@gmail.com>
2026-01-04 16:17:44 +0800

    chore: init porject

---
+++ src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1767507947430_3851',
+  koa: {
+    port: 7001,
+  },
+} as MidwayConfig;
